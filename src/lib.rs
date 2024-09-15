120fd4a34d4759be33bdc922ea7a1e0cf0121099
Wesley Moore <wes@wezm.net>
2024-09-15 21:30:47 +1000

    Add Delay type

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,8 @@ pub mod twitter;
 
 use std::env::VarError;
 use std::ffi::OsStr;
+use std::str::FromStr;
+use std::time::Duration;
 use std::{env, fmt};
 
 pub fn env_var<K: AsRef<OsStr>>(key: K) -> Result<String, ErrorMessage> {
@@ -31,6 +33,9 @@ pub fn env_var<K: AsRef<OsStr>>(key: K) -> Result<String, ErrorMessage> {
     })
 }
 
+#[derive(Debug, Eq, PartialEq)]
+struct Delay(Duration);
+
 #[derive(Debug)]
 pub struct ErrorMessage(pub String);
 
@@ -41,3 +46,63 @@ impl fmt::Display for ErrorMessage {
 }
 
 impl std::error::Error for ErrorMessage {}
+impl FromStr for Delay {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let raw = s.as_bytes();
+        match raw.last().copied() {
+            Some(c) if c == b's' || c == b'm' => {
+                let len = s.len() - 1;
+                let num_str = unsafe { std::str::from_utf8_unchecked(&raw[0..len]) };
+                let value: u16 = num_str
+                    .parse()
+                    .map_err(|_| format!("{s} is not a valid delay"))?;
+                let seconds = if c == b'm' {
+                    value
+                        .checked_mul(60)
+                        .ok_or_else(|| format!("{s} is too big"))?
+                } else {
+                    value
+                };
+                Ok(Delay(Duration::from_secs(seconds.into())))
+            }
+            Some(_) | None => Err("delay must end with 's' or 'm'".into()),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_delay_ok() {
+        let d: Delay = "10s".parse().unwrap();
+        assert_eq!(d, Delay(Duration::from_secs(10)));
+
+        let d: Delay = "10m".parse().unwrap();
+        assert_eq!(d, Delay(Duration::from_secs(10 * 60)));
+    }
+
+    #[test]
+    fn test_parse_delay_err() {
+        let d = "10".parse::<Delay>().unwrap_err();
+        assert_eq!(d, "delay must end with 's' or 'm'");
+
+        let d = "10h".parse::<Delay>().unwrap_err();
+        assert_eq!(d, "delay must end with 's' or 'm'");
+
+        let d = "-10s".parse::<Delay>().unwrap_err();
+        assert_eq!(d, "-10s is not a valid delay");
+
+        let d = "10ss".parse::<Delay>().unwrap_err();
+        assert_eq!(d, "10ss is not a valid delay");
+
+        let d = "1000000s".parse::<Delay>().unwrap_err();
+        assert_eq!(d, "1000000s is not a valid delay");
+
+        let d = "2000m".parse::<Delay>().unwrap_err();
+        assert_eq!(d, "2000m is too big");
+    }
+}
